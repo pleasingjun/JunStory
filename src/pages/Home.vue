5d31a670ec537d6261e26131596665fa02237900
Zhengjiajun <1063689044@qq.com>
2023-10-26 16:18:33 +0800

    初始化

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>11111</div>
+</template>
+
+
+<script setup>
+console.log(1)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
+
+
+const routes = [
+    {
+        path: '/',
+        name:'Home',
+        component: (()=>import('../pages/Home.vue')),
+    },
+
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
